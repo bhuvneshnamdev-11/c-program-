36422ea14ef3fa70feae54ff8e7a112542dae2ea
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-07-13 18:24:17 +0530

    update evenodd with codes

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+//to check if the number is even or odd
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("The number is even.");
+    } else {
+        printf("The number is odd.");
+    }
+
+    return 0;
+}
