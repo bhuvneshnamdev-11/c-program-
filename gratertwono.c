0749dee99e9e831a4088513ab5332d0a93d8d75e
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-07-15 00:46:00 +0530

    add program

---
+++ gratertwono.c
@@ -0,0 +1,17 @@
+//greater between two digit number with the help of nested if-else
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the value of a,b\n");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+	    printf("A is greater");
+	}
+	else
+	{
+	    printf("B is greater");
+	}
+    return 0;
+}	
