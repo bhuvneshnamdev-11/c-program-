e0e8bd2b08fb4ab73ae114023f6aee48fe6b2d63
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-07-18 18:10:15 +0530

    add program

---
+++ vowelconconant.c
@@ -0,0 +1,37 @@
+//to check weather the given charachter is vowel or consonant
+#include <stdio.h>
+int main()
+{
+    char a;
+    printf("enter a charachter: ");
+    scanf("%c",&a);
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+    {
+        printf("vowel");
+    }
+    else
+    {
+        printf("consonant");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
