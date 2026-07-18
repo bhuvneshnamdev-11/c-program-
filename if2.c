16c18dd7f288a6d85856a0609bd0349d171d744d
bhuvneshnamdev-11 <bhuvnesnamdev@gmail.com>
2026-07-18 13:45:41 +0530

    add program

---
+++ if2.c
@@ -0,0 +1,16 @@
+//if-else statement : gretaer between two nos
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter a and b");
+    scanf("%d%d",&a,&b);
+    if(a>b)//4>4
+    {
+        printf("A is Greater");
+    }
+    else
+    {  
+      printf("B is greater");
+    }
+}
