ccd4124438dc61ee8c7a3f87579954220432fc59
bhuvneshnamdev-11 <bhuvnesnamdev@gmail.com>
2026-07-18 13:46:11 +0530

    add program

---
+++ if3.c
@@ -0,0 +1,16 @@
+//if-else statement : gretaer between two nos
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter a ");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("A is Even");
+    }
+    else
+    {  
+      printf("A is Odd");
+    }
+}
