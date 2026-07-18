8bb163fdf03669cb75d821591c4f2b8379ba7573
bhuvneshnamdev-11 <bhuvnesnamdev@gmail.com>
2026-07-18 13:43:51 +0530

    add programs

---
+++ ascii.c
@@ -0,0 +1,9 @@
+//Char formate to Number Formate (Ascii value)
+#include<stdio.h>
+void main()
+{
+   char ch;
+   printf("Enter any charector");
+   scanf("%c",&ch);
+   printf("Ascii value = %d\n",ch);
+}
---
+++ ascii1.c
@@ -0,0 +1,9 @@
+// Number Formate (Ascii value) to Char formate 
+#include<stdio.h>
+void main()
+{
+   int ch;
+   printf("Enter any charector");
+   scanf("%d",&ch);
+   printf("chrector = %c\n",ch);
+}
