d95d6781dab9879b26a63a361e3829836195f1f2
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-07-20 19:17:46 +0530

    add program

---
+++ loopcount1ton.c
@@ -0,0 +1,12 @@
+//Loop Program : print counting of natural nos from 1 to n
+#include<stdio.h>
+void main()
+{
+     int n,i;
+     printf("Enter the ending of series");
+     scanf("%d",&n);
+     for(i=1;i<=n;i++)
+     {
+        printf("%d\t",i);
+        }   
+}
