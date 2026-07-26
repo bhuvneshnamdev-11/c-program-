4085626cd85665e899d2c8147b92d0436c768a59
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-07-26 12:23:23 +0530

    add program

---
+++ looppaternstar.c
@@ -0,0 +1,16 @@
+//Nested Loop : star pattern 
+#include<stdio.h>
+void main()
+{
+     int row,i,j;
+     printf("Enter the row: ");
+     scanf("%d",&row);
+     for(i=1;i<=row;i++)
+     {
+         for(j=1;j<=i;j++)
+         {
+            printf("* ");
+         }
+         printf("\n");
+     }
+}
