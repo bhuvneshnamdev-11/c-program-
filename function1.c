08532436d56a7c9012f13389870a9e6844e55680
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-07-30 12:21:01 +0530

    add program

---
+++ function1.c
@@ -0,0 +1,15 @@
+//Functions : no args and no return value
+#include<stdio.h>
+void add();
+void add()
+{
+    float a,b,sum;
+    printf("Enter the value of a and b: ");
+    scanf("%f%f",&a,&b);
+    sum = a+b;   
+    printf("Sum = %f",sum);
+}
+void main()
+{
+   add();
+}
