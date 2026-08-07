932e77d77676c9f3be9d7a5f4ff116383248105e
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-08-07 16:15:13 +0530

    add program

---
+++ 1darray4.c
@@ -0,0 +1,23 @@
+//Array input and output
+#include<stdio.h>
+void main()
+{
+   int n,i,sum=0;
+   printf("Enter the no of element: ");
+   scanf("%d",&n);
+   int a[n];
+   //input using loop
+   printf("Enter the element of array\n");
+   for(i=0;i<n;i++)
+   {
+         scanf("%d",&a[i]);
+   }
+   //dipslay using loop
+   printf("Display the element of array\n");
+   for(i=0;i<n;i++)
+   {
+         printf("%d\t",a[i]);
+         sum=sum+a[i];
+   }
+   printf("\nsum of array elements are = %d\n",sum);
+}
