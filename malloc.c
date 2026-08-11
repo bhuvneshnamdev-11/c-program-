5339d9c9dc47ddb15bc977b59c513bb52b2a716d
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-08-11 17:25:26 +0530

    add program

---
+++ malloc.c
@@ -0,0 +1,28 @@
+//Dynamic memory alloc : array 
+//malloc();
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n,*ptr,i ;
+    printf("Enter the no of elements ");
+    scanf("%d",&n);
+    ptr = malloc(n*sizeof(int));
+     if(ptr==NULL)
+     {
+	  printf("Error in memory allocation!!!");
+          exit(0);
+         //return;
+     }
+     printf("Enter the elements of array");
+     for(i=0;i<n;i++)
+     {
+        scanf("%d",(ptr+i));
+     }
+      printf("Display the elements of array\n");
+     for(i=0;i<n;i++)
+     {
+        printf("%d\t",*(ptr+i));
+     }
+     free(ptr);
+}
