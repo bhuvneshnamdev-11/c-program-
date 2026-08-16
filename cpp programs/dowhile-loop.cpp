2df5413b5389e98be8279142c6e04d6aeb90f4f2
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-08-16 13:47:29 +0530

    update changes

--- cpp programs/dowhile-loop.cpp
+++ cpp programs/dowhile-loop.cpp
@@ -12,16 +12,12 @@ int main()
 //in do while loop, the condition is checked after the execution of the loop body, 
 //so the loop will always execute at least once, even if the condition is false.
 
-    //  int i=21;
-    // do {
-    //     cout << " " << i << endl;
-    //     i++;
-    // } while (i<=5);
+    int i=21;
+    do {
+    cout << " " << i << endl;
+     i++;
+    } while (i<=5);
 
-    int i=1;
-    if(cin >> i) {
-       cout << "You entered: " << i << endl;
-    }
 
     return 0;
-} 
+}  
--- cpp programs/nested-loop.cpp
+++ cpp programs/nested-loop.cpp
@@ -6,7 +6,7 @@ int main()
     for(int i=1; i<=3; i++) {
 
         for(int j=1; j<=3; j++) {
-            cout << i << " " << j << " " << endl;
+            cout << i << j << "" << " " << endl;
         }
     }
     return 0;
