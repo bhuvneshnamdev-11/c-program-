c132597396d7105f7ffcb7b94968b4a1e3f3a12c
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-08-23 15:22:35 +0530

    add program

---
+++ cpp programs/addno.cpp
@@ -0,0 +1,14 @@
+//program to add two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout << "Enter the value of a: " << endl;
+    cin >> a;
+    cout << "Enter the vslue of b: " << endl;
+    cin >> b;
+    sum = a+b;
+    cout << "The sum of a and b is: " << sum << endl;
+    return 0;
+}
