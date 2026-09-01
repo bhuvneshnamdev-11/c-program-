30518b4e1ce2d2945fa9280d74ca2e64e6a58442
bhuvnesh namdev <bhuvnesnamdev@gmail.com>
2026-09-01 21:27:16 +0530

    add programs

---
+++ cpp programs/array/char array/vowel.cpp
@@ -0,0 +1,25 @@
+//To find vowel in charachter array
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+            str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+            str[i] == 'O' || str[i] == 'U') {
+            
+            count++;
+        }
+    }
+
+    cout << "Number of vowels = " << count;
+
+    return 0;
+}
